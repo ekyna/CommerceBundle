b32befb1a331a19e31d03146937534c461dbfd31
Etienne Dauvergne <contact@ekyna.com>
2016-08-25 13:46:35 +0200

    [Commerce] Started sale view.

--- Twig/OrderExtension.php
+++ Twig/OrderExtension.php
@@ -3,6 +3,8 @@
 namespace Ekyna\Bundle\CommerceBundle\Twig;
 
 use Ekyna\Bundle\CommerceBundle\Helper\ConstantHelper;
+use Ekyna\Component\Commerce\Common\Model\SaleInterface;
+use Ekyna\Component\Commerce\Common\View\Builder as ViewBuilder;
 
 /**
  * Class OrderExtension
@@ -16,15 +18,27 @@ class OrderExtension extends \Twig_Extension
      */
     private $constantHelper;
 
+    /**
+     * @var ViewBuilder
+     */
+    private $viewBuilder;
+
+    /**
+     * @var \Twig_Template
+     */
+    private $saleViewTemplate;
+
 
     /**
      * Constructor.
      *
      * @param ConstantHelper $constantHelper
+     * @param ViewBuilder $viewBuilder
      */
-    public function __construct(ConstantHelper $constantHelper)
+    public function __construct(ConstantHelper $constantHelper, ViewBuilder $viewBuilder)
     {
         $this->constantHelper = $constantHelper;
+        $this->viewBuilder = $viewBuilder;
     }
 
     /**
@@ -33,10 +47,57 @@ class OrderExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('order_state_label', [$this->constantHelper, 'renderOrderStateLabel'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFilter('order_state_badge', [$this->constantHelper, 'renderOrderStateBadge'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFilter(
+                'order_state_label',
+                [$this->constantHelper, 'renderOrderStateLabel'],
+                ['is_safe' => ['html']]
+            ),
+            new \Twig_SimpleFilter(
+                'order_state_badge',
+                [$this->constantHelper, 'renderOrderStateBadge'],
+                ['is_safe' => ['html']]
+            ),
+            new \Twig_SimpleFilter(
+                'sale_view',
+                [$this->viewBuilder, 'buildSaleView'],
+                ['is_safe' => ['html']]
+            ),
         ];
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction(
+                'render_sale_view',
+                [$this, 'renderSaleView'],
+                ['is_safe' => ['html'], 'needs_environment' => true]
+            ),
+        ];
+    }
+
+    /**
+     * Renders the sale view.
+     *
+     * @param \Twig_Environment $env
+     * @param SaleInterface     $sale
+     *
+     * @return string
+     */
+    public function renderSaleView(\Twig_Environment $env, SaleInterface $sale)
+    {
+        if (null === $this->saleViewTemplate) {
+            $this->saleViewTemplate = $env->loadTemplate('EkynaCommerceBundle:Common:sale_view.html.twig');
+        }
+
+        $view = $this->viewBuilder->buildSaleView($sale);
+
+        return $this->saleViewTemplate->render(['view' => $view]);
+    }
+
 
     /**
      * @inheritdoc
