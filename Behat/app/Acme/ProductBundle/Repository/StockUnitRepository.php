852661cf4bac36a4b5c0d5cc874a6f6c311ef539
Etienne Dauvergne <contact@ekyna.com>
2017-03-16 17:50:30 +0100

    [Commerce] Order (and item) stock assignments update.

--- Behat/app/Acme/ProductBundle/Repository/StockUnitRepository.php
+++ Behat/app/Acme/ProductBundle/Repository/StockUnitRepository.php
@@ -2,10 +2,10 @@
 
 namespace Acme\ProductBundle\Repository;
 
+use Acme\ProductBundle\Entity\Product;
 use Ekyna\Component\Commerce\Stock\Model\StockSubjectInterface;
 use Ekyna\Component\Commerce\Stock\Model\StockUnitStates;
 use Ekyna\Component\Commerce\Stock\Repository\StockUnitRepositoryInterface;
-use Ekyna\Component\Commerce\Supplier\Model\SupplierOrderItemInterface;
 use Ekyna\Component\Resource\Doctrine\ORM\ResourceRepository;
 
 /**
@@ -20,6 +20,10 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
      */
     public function findAvailableOrPendingBySubject(StockSubjectInterface $subject)
     {
+        if (!$subject instanceof Product) {
+            throw new \InvalidArgumentException('Expected instance of ' . Product::class);
+        }
+
         if (!$subject->getId()) {
             return [];
         }
@@ -27,7 +31,7 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
         $qb = $this->getQueryBuilder();
 
         return $qb
-            ->andWhere($qb->expr()->in('su.product', ':product'))
+            ->andWhere($qb->expr()->eq('su.product', ':product'))
             ->andWhere($qb->expr()->in('su.state', ':state'))
             ->setParameter('product', $subject)
             ->setParameter('state', [StockUnitStates::STATE_OPENED, StockUnitStates::STATE_PENDING])
@@ -38,16 +42,12 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
     /**
      * @inheritdoc
      */
-    public function findUnassignedBySubject(StockSubjectInterface $subject)
-    {
-        throw new \Exception('Not yet implemented');
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function findNewBySubject(StockSubjectInterface $subject)
     {
+        if (!$subject instanceof Product) {
+            throw new \InvalidArgumentException('Expected instance of ' . Product::class);
+        }
+
         if (!$subject->getId()) {
             return [];
         }
@@ -55,8 +55,8 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
         $qb = $this->getQueryBuilder();
 
         return $qb
-            ->andWhere($qb->expr()->in('su.product', ':product'))
-            ->andWhere($qb->expr()->in('su.state', ':states'))
+            ->andWhere($qb->expr()->eq('su.product', ':product'))
+            ->andWhere($qb->expr()->eq('su.state', ':state'))
             ->setParameter('product', $subject)
             ->setParameter('state', StockUnitStates::STATE_NEW)
             ->getQuery()
@@ -68,6 +68,10 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
      */
     public function findNotClosedSubject(StockSubjectInterface $subject)
     {
+        if (!$subject instanceof Product) {
+            throw new \InvalidArgumentException('Expected instance of ' . Product::class);
+        }
+
         if (!$subject->getId()) {
             return [];
         }
@@ -76,10 +80,38 @@ class StockUnitRepository extends ResourceRepository implements StockUnitReposit
         $qb = $this->getQueryBuilder();
 
         return $qb
-            ->andWhere($qb->expr()->in($alias . '.product', ':product'))
-            ->andWhere($qb->expr()->notIn($alias . '.state', ':state'))
+            ->andWhere($qb->expr()->eq($alias . '.product', ':product'))
+            ->andWhere($qb->expr()->neq($alias . '.state', ':state'))
             ->setParameter('product', $subject)
             ->setParameter('state', StockUnitStates::STATE_CLOSED)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function findAssignableBySubject(StockSubjectInterface $subject)
+    {
+        if (!$subject instanceof Product) {
+            throw new \InvalidArgumentException('Expected instance of ' . Product::class);
+        }
+
+        if (!$subject->getId()) {
+            return [];
+        }
+
+        $alias = $this->getAlias();
+        $qb = $this->getQueryBuilder();
+
+        return $qb
+            ->andWhere($qb->expr()->eq($alias . '.product', ':product'))
+            ->andWhere($qb->expr()->orX(
+                $qb->expr()->isNull($alias . '.supplierOrderItem'), // Not yet linked to a supplier order
+                $qb->expr()->lt($alias . '.reservedQuantity', $alias . '.orderedQuantity'),   // Reserved lower than ordered
+                $qb->expr()->eq('SIZE(' . $alias . '.stockAssignments)', 0) // No assignments TODO remove ?
+            ))
+            ->setParameter('product', $subject)
             ->getQuery()
             ->getResult();
     }
