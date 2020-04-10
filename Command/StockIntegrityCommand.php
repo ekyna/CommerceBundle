3683d6877e6a195b55ad719a4dce3114f38042bc
Etienne Dauvergne <contact@ekyna.com>
2020-04-10 14:41:31 +0200

    [Commerce] Improved stock integrity command (added email report).

--- Command/StockIntegrityCommand.php
+++ Command/StockIntegrityCommand.php
@@ -8,9 +8,13 @@ use Ekyna\Component\Commerce\Stock\Entity\AbstractStockUnit;
 use Ekyna\Component\Commerce\Stock\Integrity;
 use Ekyna\Component\Commerce\Stock\Overflow\OverflowHandlerInterface;
 use Ekyna\Component\Commerce\Stock\Updater\StockSubjectUpdaterInterface;
+use Swift_Mailer;
+use Swift_SwiftException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
@@ -44,6 +48,16 @@ class StockIntegrityCommand extends Command
     private $overflowHandler;
 
     /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
      * @var Integrity\CheckerInterface[]
      */
     private $checkers;
@@ -56,12 +70,16 @@ class StockIntegrityCommand extends Command
      * @param StockSubjectUpdaterInterface $subjectUpdater
      * @param EntityManagerInterface       $entityManager
      * @param OverflowHandlerInterface     $overflowHandler
+     * @param Swift_Mailer                 $mailer
+     * @param string                       $email
      */
     public function __construct(
         Connection $connection,
         StockSubjectUpdaterInterface $subjectUpdater,
         EntityManagerInterface $entityManager,
-        OverflowHandlerInterface $overflowHandler
+        OverflowHandlerInterface $overflowHandler,
+        Swift_Mailer $mailer,
+        string $email
     ) {
         parent::__construct();
 
@@ -69,6 +87,8 @@ class StockIntegrityCommand extends Command
         $this->subjectUpdater = $subjectUpdater;
         $this->entityManager = $entityManager;
         $this->overflowHandler = $overflowHandler;
+        $this->mailer = $mailer;
+        $this->email = $email;
     }
 
     /**
@@ -79,7 +99,7 @@ class StockIntegrityCommand extends Command
         $this
             ->setDescription('Checks the stock integrity.')
             ->addOption('fix', 'f', InputOption::VALUE_NONE, 'Whether to apply fixes')
-            ->addOption('final', null, InputOption::VALUE_NONE, 'Whether to skip final check');
+            ->addOption('report', 'r', InputOption::VALUE_NONE, 'Whether to send email report');
     }
 
     /**
@@ -92,33 +112,51 @@ class StockIntegrityCommand extends Command
             new Integrity\UnitReceivedChecker(),
             new Integrity\UnitAdjustedChecker(),
             new Integrity\AssignmentChecker(),
+            new Integrity\DuplicateChecker(),
             new Integrity\UnitAssignChecker(),
             new Integrity\UnitOverflowChecker(
                 $this->entityManager,
                 $this->overflowHandler
             ),
+            new Integrity\FinalChecker(),
         ];
 
-        if (!$input->getOption('final')) {
-            $this->checkers[] = new Integrity\FinalChecker();
+        $sendReport = $input->getOption('report');
+        $applyFix = $input->getOption('fix');
+        $noInteraction = $input->getOption('no-interaction');
+
+        if ($sendReport && $applyFix) {
+            throw new InvalidOptionException("You can't enable both report and fix.");
         }
 
-        if ($doFix = $input->getOption('fix')) {
+        if ($sendReport) {
+            $output = new BufferedOutput();
+        } elseif ($applyFix) {
+            if ($noInteraction) {
+                throw new InvalidOptionException("You can't enable fix without interaction.");
+            }
+
             // Confirm
-            $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion('<question>Do you confirm you want to apply fixes ?</question>',
-                false);
-            if (!$helper->ask($input, $output, $question)) {
+            $question = new ConfirmationQuestion(
+                '<question>Do you confirm you want to apply fixes ?</question>',
+                false
+            );
+            if (!$this->getHelper('question')->ask($input, $output, $question)) {
                 return;
             }
         }
 
         $unitIds = [];
+        $errors = false;
 
         $this->connection->beginTransaction();
         try {
-            // Check
             foreach ($this->checkers as $checker) {
+                // Skip final check if there are some errors
+                if ($checker instanceof Integrity\FinalChecker && $errors && !$applyFix) {
+                    continue;
+                }
+
                 $output->write($checker->getTitle());
 
                 $checker->setConnection($this->connection);
@@ -129,6 +167,8 @@ class StockIntegrityCommand extends Command
                     continue;
                 }
 
+                $errors = true;
+
                 $output->writeln(" <error>error</error>\n");
 
                 $checker->display($output);
@@ -137,12 +177,15 @@ class StockIntegrityCommand extends Command
 
                 $checker->build($output);
 
-                if (!$doFix) {
-                    foreach ($checker->getActions() as $action) {
-                        $output->writeln($action->getLabel());
-                    }
+                if (!$applyFix) {
+                    // Don't log actions into email report
+                    if (!$sendReport) {
+                        foreach ($checker->getActions() as $action) {
+                            $output->writeln($action->getLabel());
+                        }
 
-                    $output->writeln('');
+                        $output->writeln('');
+                    }
 
                     continue;
                 }
@@ -156,15 +199,41 @@ class StockIntegrityCommand extends Command
             $this->connection->commit();
         } catch (\Throwable $e) {
             $this->connection->rollBack();
-            $this->connection->close();
 
-            throw $e;
+            $unitIds = []; // Clear unit ids to prevent subject update
+
+            if ($sendReport) {
+                $output->writeln("\n{$e->getMessage()}\n");
+                $errors = true;
+            } else {
+                $this->connection->close();
+                throw $e;
+            }
         }
 
         // Updates subjects
         $this->updateSubjects($output, $unitIds);
 
         $this->connection->close();
+
+        if (!($sendReport && $errors)) {
+            return;
+        }
+
+        $message =
+            \Swift_Message::newInstance(
+                'Stock integrity report',
+                $output->fetch(),
+                'text/plain'
+            )
+            ->setFrom($this->email)
+            ->setTo($this->email);
+
+        try {
+            $this->mailer->send($message);
+        } /** @noinspection PhpRedundantCatchClauseInspection */ catch (Swift_SwiftException $e) {
+            // In case transport has bad configuration.
+        }
     }
 
     /**
