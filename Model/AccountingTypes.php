71c0d31dbba18057fb649637039dd9e19935cb0d
Etienne Dauvergne <contact@ekyna.com>
2020-09-22 10:15:52 +0200

    [Commerce] Accounting: handle invoice items.

--- Model/AccountingTypes.php
+++ Model/AccountingTypes.php
@@ -20,14 +20,15 @@ final class AccountingTypes extends AbstractConstants
         $prefix = 'ekyna_commerce.accounting.type.';
 
         return [
-            Types::TYPE_GOOD     => [$prefix . Types::TYPE_GOOD],
+            Types::TYPE_GOOD       => [$prefix . Types::TYPE_GOOD],
             //Types::TYPE_SERVICE  => [$prefix . Types::TYPE_SERVICE],
-            Types::TYPE_SHIPPING => [$prefix . Types::TYPE_SHIPPING],
-            Types::TYPE_TAX      => [$prefix . Types::TYPE_TAX],
-            Types::TYPE_PAYMENT  => [$prefix . Types::TYPE_PAYMENT],
-            Types::TYPE_UNPAID   => [$prefix . Types::TYPE_UNPAID],
-            Types::TYPE_EX_GAIN  => [$prefix . Types::TYPE_EX_GAIN],
-            Types::TYPE_EX_LOSS  => [$prefix . Types::TYPE_EX_LOSS],
+            Types::TYPE_SHIPPING   => [$prefix . Types::TYPE_SHIPPING],
+            Types::TYPE_TAX        => [$prefix . Types::TYPE_TAX],
+            Types::TYPE_PAYMENT    => [$prefix . Types::TYPE_PAYMENT],
+            Types::TYPE_UNPAID     => [$prefix . Types::TYPE_UNPAID],
+            Types::TYPE_EX_GAIN    => [$prefix . Types::TYPE_EX_GAIN],
+            Types::TYPE_EX_LOSS    => [$prefix . Types::TYPE_EX_LOSS],
+            Types::TYPE_ADJUSTMENT => [$prefix . Types::TYPE_ADJUSTMENT],
         ];
     }
 
