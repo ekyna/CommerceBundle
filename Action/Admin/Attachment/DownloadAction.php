c47dcce0d330c54818031219b33204f8abb7281d
Étienne Dauvergne <contact@ekyna.com>
2025-10-07 17:25:45 +0200

    [Commerce] Use abstract download action from admin bundle.

--- Action/Admin/Attachment/DownloadAction.php
+++ Action/Admin/Attachment/DownloadAction.php
@@ -4,48 +4,16 @@ declare(strict_types=1);
 
 namespace Ekyna\Bundle\CommerceBundle\Action\Admin\Attachment;
 
-use Ekyna\Bundle\AdminBundle\Action\AdminActionInterface;
-use Ekyna\Bundle\ResourceBundle\Action\AbstractAction;
-use Ekyna\Bundle\ResourceBundle\Service\Filesystem\FilesystemHelper;
-use Ekyna\Component\Commerce\Common\Model\AttachmentInterface;
-use Ekyna\Component\Commerce\Exception\UnexpectedTypeException;
+use Ekyna\Bundle\AdminBundle\Action\AbstractDownloadAction;
 use Ekyna\Component\Resource\Action\Permission;
-use League\Flysystem\Filesystem;
-use League\Flysystem\FilesystemException;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class DownloadAction
  * @package Ekyna\Bundle\CommerceBundle\Action\Admin\Attachment
  * @author  Étienne Dauvergne <contact@ekyna.com>
  */
-class DownloadAction extends AbstractAction implements AdminActionInterface
+class DownloadAction extends AbstractDownloadAction
 {
-    private Filesystem $filesystem;
-
-    public function __construct(Filesystem $filesystem)
-    {
-        $this->filesystem = $filesystem;
-    }
-
-    public function __invoke(): Response
-    {
-        $resource = $this->context->getResource();
-
-        if (!$resource instanceof AttachmentInterface) {
-            throw new UnexpectedTypeException($resource, AttachmentInterface::class);
-        }
-
-        $helper = new FilesystemHelper($this->filesystem);
-
-        try {
-            return $helper->createFileResponse($resource->getPath());
-        } catch (FilesystemException $exception) {
-        }
-
-        return new Response('File does not exist or is not available', Response::HTTP_NOT_FOUND);
-    }
-
     public static function configureAction(): array
     {
         return [
